d250b1f592455f529653c85faa55c0f5b3b2fa4e
Ваня Х <dfdiz@mail.ru>
2026-07-06 14:27:43 +0000

    fix timers, add SW notification handler, update icons

--- sw.js
+++ sw.js
@@ -43,3 +43,16 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
+self.addEventListener('notificationclick', (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+      if (clientList.length > 0) {
+        clientList[0].focus();
+      } else {
+        clients.openWindow('./');
+      }
+    })
+  );
+});
