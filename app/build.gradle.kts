b3eef6f23a54a41d935b8ca872c900034c743ecc
Ваня Х <dfdiz@mail.ru>
2026-07-08 13:04:43 +0000

    versionCode=10 versionName=1.2.0, fix ternary bugs, rewrite Help section, disable auto-notify prompt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.chistiyen.app"
         minSdk = 26
         targetSdk = 29
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 10
+        versionName = "1.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
